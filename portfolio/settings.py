11b31afefc6940f80e985905be6941c5b306a495
Andre-Ovien <andreovien571@gmail.com>
2025-04-07 17:55:52 +0100

    created media file in django

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'employee',
+    'index',
 ]
 
 MIDDLEWARE = [
@@ -124,6 +125,10 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR / 'assets')
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
