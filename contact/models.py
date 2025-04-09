3600c931abc94d540a3514308216ccca5f40a4f4
Andre-Ovien <andreovien571@gmail.com>
2025-04-09 16:26:51 +0100

    modified contact template to allow data insertion

--- contact/models.py
+++ contact/models.py
@@ -8,4 +8,14 @@ class ContactList(models.Model):
 
     def __str__(self):
         return self.email
+
+
+class ContactForm(models.Model):
+    name = models.CharField(max_length=100, blank= False)
+    email = models.CharField(max_length=100, blank= False)
+    subject = models.CharField(max_length=400, blank= False)
+    message = models.TextField(max_length=800, blank= False)
+    
+    def __str__(self):
+        return self.name       
     
--- contact/views.py
+++ contact/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from .models import ContactList
+from .models import ContactForm
 
 # Create your views here.
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        contactformdata = ContactForm(name=name,email=email,subject=subject,message=message)
+        contactformdata.save()
+
     contactdata = ContactList.objects.all()[0]
     contexts = {
         'contacts': contactdata
